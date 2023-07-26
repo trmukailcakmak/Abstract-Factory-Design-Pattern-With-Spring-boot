f915b153c718f5ae91576f732beafd0db3da2a1e
mukail cakmak <tr.mukailcakmak@gmail.com>
2023-07-26 17:54:08 +0300

    [Mukail Cakmak] [main] main class area added more information

--- src/main/java/com/cakmak/designPattern/abstractFactory/Application.java
+++ src/main/java/com/cakmak/designPattern/abstractFactory/Application.java
@@ -14,20 +14,22 @@ public class Application {
 
 
 		ICar audiCar = audiFactory.createCar("Beyaz", 500000);
-		ICar bmwCar  = bmwFactory.createCar("Siyah", 420000);
+		ICar bmwCar = bmwFactory.createCar("Siyah", 420000);
 
 		IMotorCycle audiMotorCycle = audiFactory.createMotorCycle("Yarış Motoru", 125000);
 		IMotorCycle bmwMotorCycle = bmwFactory.createMotorCycle("Cadde Motoru", 24000);
 
-		System.out.println("Audi Car Color: "+audiCar.getColor());
-		System.out.println("Audi Car Price: "+audiCar.getPrice());
+		System.out.println("Audi Car Color: " + audiCar.getColor());
+		System.out.println("Audi Car Price: " + audiCar.getPrice());
 		System.out.println();
-		System.out.println("BMW Car Color: "+bmwCar.getColor());
-		System.out.println("BMW Car Price: "+bmwCar.getPrice());
+		System.out.println("BMW Car Color: " + bmwCar.getColor());
+		System.out.println("BMW Car Price: " + bmwCar.getPrice());
 		System.out.println();
-		System.out.println("Audi Motor Cycle Price: "+audiMotorCycle.getPrice());
+		System.out.println("Audi Motor Cycle Type: " + audiMotorCycle.getType());
+		System.out.println("Audi Motor Cycle Price: " + audiMotorCycle.getPrice());
 		System.out.println();
-		System.out.println("BMW Motor Cycle Price: "+bmwMotorCycle.getPrice());
+		System.out.println("BMW Motor Cycle Type: " + bmwMotorCycle.getType());
+		System.out.println("BMW Motor Cycle Price: " + bmwMotorCycle.getPrice());
 	}
 
 }
